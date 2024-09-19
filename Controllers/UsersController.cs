a616cfc019af0c97087a6d17fdc37b23b3e24312
Rana Deek <ranadeek2002@gmail.com>
2024-09-20 01:00:11 +0300

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Register_Login_Task.Data;
+using Register_Login_Task.Models;
+
+namespace Register_Login_Task.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignIn(User model)
+        {
+
+            var user = _context.Users.FirstOrDefault(u => u.Email == model.Email);
+            if (user == null)
+            {
+                Console.WriteLine("User not found");
+            }
+            else
+            {
+                if(user.Email == model.Email && user.Password == model.Password)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return RedirectToAction(nameof(SignIn));
+                }
+                
+            }
+
+            return View(model);
+        }
+        public IActionResult ChangeStatus(int id)
+        {
+            var user = _context.Users.Find(id);
+            user.Active =!user.Active;
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace Register_Login_Task.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Display(Name = "User Name")]
+        [DataType("varchar")]
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "User Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [MaxLength(20)]
+        [Required]
+        public string Password { get; set; }
+        public bool Active { get; set; }
+    }
+}
